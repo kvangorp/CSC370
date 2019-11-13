8c249d8d9e4593638fed74022c7f2fe8868eb840
daytondep <dayton_depradine@yahoo.ca>
2019-11-12 21:52:20 -0800

    .

--- db.py
+++ db.py
@@ -22,9 +22,9 @@ conn = psycopg2.connect(dbname=psql_db,
 
 #dont know if this  works or not just writing stuff
 
-def selectFromDB(values,table) #not complete just rough skeleton
+def selectFromDB(values,table,conditions) #not complete just rough skeleton
     cur = conn.cursor()
-    string = "SELECT %s FROM %s ;" (values,table)
+    string = "SELECT %s FROM %s WHERE %s;" (values,table,conditions)
     cur.execute(string)
     rows = cur.fetchall()
     for row in rows:
