9a2260046e87394012e82804bb34cac2ad9b3664
tramung <tram.ungtt@gmail.com>
2019-11-18 15:26:38 -0800

    add update and delete functions

--- db.py
+++ db.py
@@ -8,22 +8,39 @@ psql_password = 'V00882488'
 psql_server = 'studentdb1.csc.uvic.ca'
 psql_port = 5432
 conn = psycopg2.connect(dbname=psql_db,
-    user=psql_user,
-    password=psql_password,
-    host=psql_server,
-    port=psql_port) 
+  user=psql_user,
+  password=psql_password,
+  host=psql_server,
+  port=psql_port) 
 
 #dont know if this  works or not just writing stuff
 #update : works!
 def selectFromDB(values,table,conditions):
-        cur = conn.cursor()
-        string = "SELECT %s FROM %s %s;" %(values,table,conditions)
-        cur.execute(string)
-        rows = cur.fetchall()
-        for row in rows:
-            print(row)
+  cur = conn.cursor()
+  string = "SELECT %s FROM %s %s;" %(values,table,conditions)
+  cur.execute(string)
+  rows = cur.fetchall()
+  for row in rows:
+    print(row)
+
+def deleteFromDB(table, conditions):
+  cur = conn.cursor()
+  query = "DELETE FROM %s %s;" %(table, conditions)
+  cur.execute(query)
+  rows = cur.fetchall()
+  for row in rows:
+    print(row)
+
+def updateFromDB(table, conditions):
+  cur = conn.cursor()
+  query = "UPDATE %s SET %s %s;" %(table, set, conditions)
+  cur.execute(query)
+  rows = cur.fetchall()
+  for row in rows:
+    print(row)
 
 #Function calls
 selectFromDB('*','users','')
-
+deleteFromDB()
+updateFromDB()
 
